0d6dc44d7a50eb2dd2a5d6398d0a9aa1f808d5ca
qiannian <40210590@qq.com>
2020-04-02 00:28:25 +0800

    example

---
+++ examples/windows/go.mod
@@ -0,0 +1,5 @@
+module examples/dm
+
+go 1.14
+
+require github.com/qianniancn/go-dmsoft v0.0.0-20200401152246-cd85833de2a8 // indirect
---
+++ examples/windows/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/qianniancn/go-dmsoft"
+)
+
+func main() {
+	// 使用免注册方式注册大漠插件，也可以使用命令行注册
+
+	dmsoft.SetDllPathW("./dm.dll", 0)
+	dm, _ := dmsoft.New()
+	defer dm.Release() //必须释放
+	fmt.Printf("当前插件版本:%s\n", dm.Ver())
+	if dm.Capture(0, 0, 600, 500, "./test.png") == 1 {
+		fmt.Println("截图成功")
+	}
+	// 打开记事本
+	if dm.RunApp("C:\\Windows\\System32\\notepad.exe", 0) == 1 {
+		dm.Beep(500, 500) // 蜂鸣
+		dm.Delay(2000)
+		hwnd1 := dm.FindWindow("", "记事本")
+		hwnd2, _ := strconv.Atoi(dm.EnumWindow(hwnd1, "", "Edit", 2))
+		fmt.Println(hwnd1, hwnd2)
+
+		for i := 1; i < 10; i++ {
+			dm.SendString(hwnd2, "hell world\n")
+		}
+	}
+}
