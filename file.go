0c101d262ace75041783f50ed0999461694779e6
qiannian <40210590@qq.com>
2020-04-01 23:12:25 +0800

    文件操作

--- file.go
+++ file.go
@@ -1,3 +1,30 @@
-// 文件
+// 文件操作可以通过go来实现
 
 package dmsoft
+
+// long CopyFile(src_file,dst_file,over)
+// long CreateFolder(folder)
+// long DecodeFile(file,pwd)
+// long DeleteFile(file)
+// long DeleteFolder(folder)
+// long DeleteIni(section,key,file)
+// long DeleteIniPwd(section,key,file,pwd)
+// long DownloadFile(url,save_file,timeout)
+// long EncodeFile(file,pwd)
+// string EnumIniKey(section,file)
+// string EnumIniKeyPwd(section,file,pwd)
+// string EnumIniSection(file)
+// string EnumIniSectionPwd(file,pwd)
+// long GetFileLength(file)
+// string GetRealPath(path)
+// long IsFileExist(file)
+// long IsFolderExist (folder)
+// long MoveFile(src_file,dst_file)
+// string ReadFile(file)
+// string ReadIni(section,key,file)
+// string ReadIniPwd(section,key,file,pwd)
+// string SelectDirectory()
+// string SelectFile()
+// long WriteFile(file,content)
+// long WriteIni(section,key,value,file)
+// long WriteIniPwd(section,key,value,file,pwd)
